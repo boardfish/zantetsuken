d6a7ba262c8c244571cdfed5f0fcfc6651c92fe2
Simon Fish <si@mon.fish>
2021-07-08 17:50:57 +0100

    Add ReCAPTCHA ruleset

---
+++ app/lib/content_security_policy/ruleset/recaptcha.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module ContentSecurityPolicy
+  module Ruleset
+    # For verification that our users aren't bots.
+    class Recaptcha < Base
+      def script_src
+        ['https://www.google.com/recaptcha/', 'https://www.gstatic.com/recaptcha/']
+      end
+
+      def frame_src
+        ['https://www.google.com/recaptcha/', 'https://recaptcha.google.com/recaptcha/']
+      end
+    end
+  end
+end
