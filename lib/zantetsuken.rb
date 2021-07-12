af318fee26b915abc4689e6a4d6cee3b2f84eddc
Simon Fish <si@mon.fish>
2021-07-12 14:58:13 +0100

    Remove references to baked-in configuration

--- lib/zantetsuken.rb
+++ lib/zantetsuken.rb
@@ -8,18 +8,6 @@ require 'active_support/core_ext/string/inquiry'
 
 require_relative 'zantetsuken/ruleset'
 require_relative 'zantetsuken/ruleset/base'
-require_relative 'zantetsuken/ruleset/adobe_document_cloud'
-require_relative 'zantetsuken/ruleset/airtable'
-require_relative 'zantetsuken/ruleset/google_analytics'
-require_relative 'zantetsuken/ruleset/intercom'
-require_relative 'zantetsuken/ruleset/minio'
-require_relative 'zantetsuken/ruleset/recaptcha'
-require_relative 'zantetsuken/ruleset/site'
-require_relative 'zantetsuken/ruleset/stripe'
-require_relative 'zantetsuken/ruleset/timekit'
-require_relative 'zantetsuken/ruleset/twitch'
-require_relative 'zantetsuken/ruleset/typeform'
-require_relative 'zantetsuken/ruleset/webpack_dev_server'
 
 # A policy that defines sources from which content should be able to load in the
 # browser when you visit this Rails application.
